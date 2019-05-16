db689262e473b1a9297246362d7a977cea969af7
rvndrupal <rodrigodrupal1@gmail.com>
2019-05-15 23:48:37 -0500

    Comunidad Estilos

--- resources/views/front/comunidades.blade.php
+++ resources/views/front/comunidades.blade.php
@@ -4,17 +4,33 @@
 
         @foreach ($comunidad as $com)
 
-            <div class="col-sm-6 col-md-3 col-lg-3 col-xl-3 comunidad">
-                <!-- normal -->
-                <div class="ih-item circle effect7 right_to_left"><a href="{{ $com->link }}">
-                    <div class="img"><img src="{{ $com->imagen }}" alt="img"></div>
-                    <div class="info">
-                    <h3>{{ $com->titulo }}</h3>
-                    <p>{{ $com->descripcion }}</p>
-                    </div></a></div>
-                <!-- end normal -->
 
-            </div>
+                @if($loop->index <= 3)
+
+                    <div class="col-sm-6 col-md-3 col-lg-3 col-xl-3 comunidad   {{ $loop->index }}">
+                        <div class="ih-item circle effect7 right_to_left"><a href="{{ $com->link }}">
+                                <div class="img"><img src="{{ $com->imagen }}" alt="img"></div>
+                                <div class="info">
+                                <h3>{{ $com->titulo }}</h3>
+                                <p>{{ $com->descripcion }}</p>
+                                </div></a></div>
+                            <!-- end normal -->
+
+                @else
+                <div class="col-sm-6 col-md-3 col-lg-3 col-xl-3 comunidad-izq">
+                    <div class="ih-item circle effect7 right_to_left"><a href="{{ $com->link }}">
+                            <div class="img"><img src="{{ $com->imagen }}" alt="img"></div>
+                            <div class="info">
+                            <h3>{{ $com->titulo }}</h3>
+                            <p>{{ $com->descripcion }}</p>
+                            </div></a></div>
+                        <!-- end normal -->
+
+
+                @endif
+
+
+</div>
         @endforeach
 
 
